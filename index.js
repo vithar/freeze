140793bd2880a82553db67ae2570dcb5835e7df0
Vitaly Harisov <vitaly@harisov.name>
2016-04-30 20:14:52 +0300

    Code reformatted

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var path = require('path'),
-	borschikFreeze = require('borschik/lib/freeze')
+    borschikFreeze = require('borschik/lib/freeze');
 
 module.exports = {
-	file: function file(options) {
-		return function(url) {
-			if (url.indexOf('http') === 0) {
-				return url;
-			}
+    file: function file(options) {
+        return function(url) {
+            if (url.indexOf('http') === 0) {
+                return url;
+            }
 
-		    var absFilePath = path.resolve(options.folder, url);
+            var absFilePath = path.resolve(options.folder, url);
 
-		    return borschikFreeze.freeze(absFilePath).replace(path.resolve(options.cutPrefix), '');
-		};
-	};
+            return borschikFreeze.freeze(absFilePath).replace(path.resolve(options.cutPrefix), '');
+        };
+    }
 }
 
