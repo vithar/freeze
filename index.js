c103519ade9b0d0cfba3533ff2854c11fa949075
Vitaly Harisov <vitaly@harisov.name>
2016-04-30 19:24:54 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,17 @@
+var path = require('path'),
+	borschikFreeze = require('borschik/lib/freeze')
+
+module.exports = {
+	file: function file(options) {
+		return function(url) {
+			if (url.indexOf('http') === 0) {
+				return url;
+			}
+
+		    var absFilePath = path.resolve(options.folder, url);
+
+		    return borschikFreeze.freeze(absFilePath).replace(path.resolve(options.cutPrefix), '');
+		};
+	};
+}
+
